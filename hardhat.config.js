ece50a70916a8f56a05208172822b744264d5c07
raihanwebmaster <raihanemon2015@gmail.com>
2022-03-19 17:17:45 +0100

    web3 krypt smart contract

---
+++ hardhat.config.js
@@ -0,0 +1,12 @@
+
+require('@nomiclabs/hardhat-waffle');
+
+module.exports={
+  solidity: '0.8.0',
+  networks:{
+    ropsten:{
+      url:'https://eth-ropsten.alchemyapi.io/v2/-qpbsZ-Zt4LlLOCU_pVhP_FaPeZ_FNJZ',
+      accounts:['3cbcdd0011fbe6b3c412acd74d6beca26d70a9f76c2ece3a96574a8c9bdaf6d7']
+    }
+  }
+}
